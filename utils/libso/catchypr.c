1619e035e3affc7e40402b39faec9f25d07d36e7
Zeronetsec <senxhunter996@gmail.com>
2026-08-28 16:46:40 +0700

    modified: utils/libso/catchypr.c

--- utils/libso/catchypr.c
+++ utils/libso/catchypr.c
@@ -5,20 +5,10 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <libgen.h>
-
-typedef struct word_desc {
-    char *word;
-    int flags;
-} WORD_DESC;
-
-typedef struct word_list {
-    struct word_list *next;
-    WORD_DESC *word;
-} WORD_LIST;
+#include <builtins.h>
+#include <shell.h>
 
 int catchypr_builtin(WORD_LIST *list);
-char *get_string_value(const char *name);
 
 int catchypr_builtin(WORD_LIST *list) {
     char *exit_str = get_string_value("exit_code");
@@ -29,58 +19,110 @@ int catchypr_builtin(WORD_LIST *list) {
     char *pwd = get_string_value("PWD");
     if (!pwd) pwd = "/";
 
-    char pwd_copy[1024];
-    strncpy(
-        pwd_copy,
-        pwd,
-        sizeof(pwd_copy) - 1
-    );
-    pwd_copy[sizeof(pwd_copy) - 1] = '\0';
+    char *home = get_string_value("HOME");
 
-    char *bname = basename(pwd_copy);
-    char cpath[1024];
+    char *dirtrim_str = get_string_value("PROMPT_DIRTRIM");
+    int dirtrim = dirtrim_str ?
+        atoi(dirtrim_str) :
+        0;
 
-    if (strcmp(pwd, "/") == 0) {
+    char working_path[2048];
+    int is_home = 0;
+    size_t home_len = home ?
+        strlen(home) :
+        0;
+
+    if (
+        home &&
+        strncmp(pwd, home, home_len) == 0 && 
+       (
+            pwd[home_len] == '/' ||
+            pwd[home_len] == '\0'
+        )
+    ) {
         snprintf(
-            cpath,
-            sizeof(cpath),
-            "~/"
+            working_path,
+            sizeof(working_path),
+            "~%s",
+            pwd + home_len
         );
+        is_home = 1;
     } else {
         snprintf(
-            cpath,
-            sizeof(cpath),
-            "~/%s",
-            bname
+            working_path,
+            sizeof(working_path),
+            "%s",
+            pwd
         );
     }
 
-    char *path_color = "\001\033[0;33m\002";
+    char cpath[2048];
+    if (dirtrim > 0) {
+        int slashes_found = 0;
+        int len = strlen(working_path);
+        int i = len - 1;
+
+        if (i > 0 && working_path[i] == '/') i--;
+        for (; i >= 0; i--) {
+            if (working_path[i] == '/') {
+                slashes_found++;
+                if (slashes_found == dirtrim) break;
+            }
+        }
+
+        int prefix_len = is_home ? 1 : 0;
+        if (i > prefix_len) {
+            if (is_home) {
+                snprintf(
+                    cpath,
+                    sizeof(cpath),
+                    "~/...%s",
+                    &working_path[i]
+                );
+            } else {
+                snprintf(
+                    cpath,
+                    sizeof(cpath),
+                    "/...%s",
+                    &working_path[i]
+                );
+            }
+        } else {
+            strncpy(
+                cpath,
+                working_path,
+                sizeof(cpath)
+            );
+        }
+    } else {
+        strncpy(
+            cpath,
+            working_path,
+            sizeof(cpath)
+        );
+    }
+
+    char *path_color = "\001\x1b[0;33m\002";
     if (
-        strcmp(cpath, "~/") == 0 ||
-        strcmp(cpath, "~/root") == 0
+        strcmp(pwd, "/") == 0 ||
+        strncmp(pwd, "/root", 5) == 0
     ) {
-        path_color = "\001\033[0;31m\002";
+        path_color = "\001\x1b[0;31m\002";
     } else if (
-        strcmp(cpath, "~/etc") == 0
+        strncmp(pwd, "/etc", 4) == 0
     ) {
-        path_color = "\001\033[0;32m\002";
+        path_color = "\001\x1b[0;32m\002";
     } else if (
-        strcmp(cpath, "~/usr") == 0
+        strncmp(pwd, "/usr", 4) == 0
     ) {
-        path_color = "\001\033[0;34m\002";
-    } else if (
-        strcmp(cpath, "~/share") == 0
-    ) {
-        path_color = "\001\033[0;36m\002";
+        path_color = "\001\x1b[0;34m\002";
     }
 
     time_t now = time(NULL);
     struct tm *tm_info = localtime(&now);
-    int hour = tm_info->tm_hour;
 
     char *symbol1 = "𖤍";
-    switch (hour) {
+    switch (tm_info->tm_hour) {
         case 0: symbol1 = "☾"; break;
         case 1: symbol1 = "⋄"; break;
         case 5: symbol1 = "⚔"; break;
@@ -89,8 +131,7 @@ int catchypr_builtin(WORD_LIST *list) {
         case 20: symbol1 = "𖤐"; break;
     }
 
-    int is_root = (geteuid() == 0);
-    char *sss = is_root ?
+    char *sss = (geteuid() == 0) ?
         "#" :
         "$";
 
@@ -101,34 +142,36 @@ int catchypr_builtin(WORD_LIST *list) {
         snprintf(
             symbol2,
             sizeof(symbol2),
-            "\001\033[38;5;244m\002%s",
+            "\001\x1b[38;5;244m\002%s",
             symbol1
         );
 
         snprintf(
             symbol3,
             sizeof(symbol3),
-            "\001\033[38;5;252m\002%s",
+            "\001\x1b[38;5;252m\002%s",
             sss
         );
     } else {
         snprintf(
             symbol2,
             sizeof(symbol2),
-            "\001\033[1;34m\002%s",
+            "\001\x1b[1;34m\002%s",
             symbol1
         );
 
         snprintf(
             symbol3,
             sizeof(symbol3),
-            "\001\033[1;31m\002%s",
+            "\001\x1b[1;31m\002%s",
             sss
         );
     }
 
     printf(
-        "\001\033[?25h\002\n\001\033[38;5;244m\002┌──(\001\033[1;31m\002Ares\001\033[38;5;244m\002(%s\001\033[38;5;244m\002)\001\033[1;31m\002Framework\001\033[38;5;244m\002)-(%s%s\001\033[38;5;244m\002)\n\001\033[38;5;244m\002└──%s\001\033[0;32m\002 ", 
+        "\001\x1b[?25h\002\n"
+        "\001\x1b[38;5;244m\002┌──(\001\x1b[1;31m\002Ares\001\x1b[38;5;244m\002(%s\001\x1b[38;5;244m\002)\001\x1b[1;31m\002Framework\001\x1b[38;5;244m\002)-(%s%s\001\x1b[38;5;244m\002)\n"
+        "\001\x1b[38;5;244m\002└──%s\001\x1b[0;32m\002 ", 
         symbol2, path_color, cpath, symbol3
     );
 
@@ -140,14 +183,7 @@ char *catchypr_doc[] = {
     (char *)NULL
 };
 
-struct builtin {
-    char *name;
-    int (*function)(WORD_LIST *);
-    int flags;
-    char **long_doc;
-    char *short_doc;
-    char *handle;
-} catchypr_struct = {
+struct builtin catchypr_struct = {
     "catchypr",
     catchypr_builtin,
     1,
